4aeaa182e16c38f101d69b506652390fcde43400
Siddhant <109289709+siddhant7666@users.noreply.github.com>
2024-04-20 02:14:45 +0000

    ho gya print all pairs bhi

---
+++ Array Level - 2/print_all_pairs.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print_pairs(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<" "<< arr[i]<<" "<<arr[j]<<endl;
+        }
+    }
+
+
+}
+int main()
+{
+    int arr[3]={0,1,2};
+    print_pairs(arr,3);
+
+}
