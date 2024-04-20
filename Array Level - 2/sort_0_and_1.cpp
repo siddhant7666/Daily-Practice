f44afd97d19157f2caa9fcbe2bee03207f308ca5
Siddhant <109289709+siddhant7666@users.noreply.github.com>
2024-04-20 02:33:09 +0000

    abhi baki  hai ye solve karna sort 0 and 1

---
+++ Array Level - 2/sort_0_and_1.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void sort_array(int arr[], int n)
+{
+    // 1st approach : number of zeros aur 1 count kar lo aur
+    // uske hissab se array m daal do....sort ho jayega simple.
+
+    //2nd approach : TWO POINTER METHOD 
+
+    int start =0;
+    int end = n-1;
+
+    while(start<=end)
+    {
+        if(arr[start]==1)
+        {
+            swap(arr[start],arr[end]);
+            
+            
+        }
+        else if(arr[end]==0)
+        {
+            swap(arr[start],arr[end]);
+            start++;
+            end--;
+
+        }
+    }
+
+}
+
+int main()
+{
+    int arr[10]={0,1,0,0,0,1,1,0,1,0};
+    sort_array(arr,10);
+}
