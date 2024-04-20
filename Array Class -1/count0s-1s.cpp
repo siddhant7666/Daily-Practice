10b9d194febac3232d90fb1b73805c92f97334c3
Siddhant <109289709+siddhant7666@users.noreply.github.com>
2024-04-20 01:06:04 +0000

    Created a new folder for array class 1 and solved zero and ones count question in it

---
+++ Array Class -1/count0s-1s.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int arr[15]={1,0,1,2,0,0,1,2,1,0,1,1,0,0,1};
+    int zero_count =0;
+    int one_count =0;
+    for(int i=0; i<15; i++)
+    {
+        if(arr[i]==0)
+        zero_count++;
+        else if(arr[i]==1)
+        one_count++;
+        
+    }
+
+    cout<<"Number of Zeros are: "<<zero_count<<endl;
+    cout<<"Number of Ones are: "<<one_count;
+
+}
