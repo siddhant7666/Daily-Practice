f59f2ebc2035a2306a0e3897a40734699483861a
Siddhant <109289709+siddhant7666@users.noreply.github.com>
2024-04-26 02:30:12 +0000

    sort kar diya 2 pointer se

--- Array Level - 2/sort_0_and_1.cpp
+++ Array Level - 2/sort_0_and_1.cpp
@@ -6,26 +6,33 @@ void sort_array(int arr[], int n)
     // 1st approach : number of zeros aur 1 count kar lo aur
     // uske hissab se array m daal do....sort ho jayega simple.
 
-    //2nd approach : TWO POINTER METHOD 
+    //2nd approach : TWO POINTER METHOD -> TC : 0(N) , SC : 
 
     int start =0;
     int end = n-1;
 
-    while(start<=end)
+    while(start<end)
     {
-        if(arr[start]==1)
+        if(arr[start]==0 )
         {
-            swap(arr[start],arr[end]);
-            
-            
+            start++;
         }
-        else if(arr[end]==0)
+        else if (arr[end]==1)
+        {
+            end--;
+        }
+        else if( arr[start]==1 && arr[end]==0) // else
         {
             swap(arr[start],arr[end]);
             start++;
-            end--;
-
+            end--;  
         }
+       
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        cout<<" "<<arr[i]<<" ";
     }
 
 }
