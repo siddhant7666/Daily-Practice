0247d94be0baef175fa4ef5ac6ab945622b36334
Siddhant <dearsidd2017@gmail.com>
2024-07-14 16:30:14 +0530

    zero and one count in test file

--- Test.cpp
+++ Test.cpp
@@ -1,7 +1,24 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 int main()
 {
+    int n;
+    cin>>n;
+
+    int arr[n]={1,0,0,0,1,0,1,0,1,0,0,0,1,1,2,3,4,5,0,1,0};
+    int zero_count=0;
+    int one_count=0;
+    for(int i=0; i<n;i++)
+    {
+        if(arr[i]==0)
+        zero_count++;
+        else if(arr[i]==1)
+        one_count++;
+    }
+
+    cout<<"The number of zeroes in this program is: "<<zero_count;
+    cout<<endl;
+    cout<<"The number of ones in this program is: "<<one_count;
 
 
 }
