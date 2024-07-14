f6057c2f6249b4892c5f9eadbe56704a75307747
Siddhant <dearsidd2017@gmail.com>
2024-07-14 17:14:14 +0530

    reverse an array-test

--- Test.cpp
+++ Test.cpp
@@ -1,40 +1,28 @@
 #include<iostream>
-#include<climits>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the size of array: ";
     cin>>n;
-    //dynamically creating the array
-
     int* arr = new int[n];
-    cout<<"Enter the values of array: ";
     for(int i=0; i<n; i++)
     {
-        
         cin>>arr[i];
-        
     }
 
-    // solving the problem:
+    int start =0;
+    int end = n-1;
 
-    int min = INT_MAX;
-    int max = INT_MIN;
+    while(start<=end)
+    {
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
 
     for(int j=0; j<n; j++)
     {
-        if(arr[j]> max)
-        max = arr[j];
-
-        else if(arr[j]<min)
-        min = arr[j];
+        cout<<arr[j]<<" ";
     }
 
-    cout<<"Maximum value in this array is: "<<max;
-    cout<<endl;
-    cout<<"Minimum value in this array is: "<<min;
-
-    delete[] arr;
-    return 0;
 }
