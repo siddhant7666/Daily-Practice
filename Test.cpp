586ee9edb4c3605c93ed46d7e9ca1238b9bacd76
Siddhant <dearsidd2017@gmail.com>
2024-07-15 20:23:46 +0530

    shifting elements by left

--- Test.cpp
+++ Test.cpp
@@ -181,21 +181,21 @@
 //     }
 // }
 
-#include<iostream>
-using namespace std;
-int main()
-{
-    int arr[6]={10,20,30,40,50,60};
-    int temp = arr[0];
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[6]={10,20,30,40,50,60};
+//     int temp = arr[0];
 
-    for(int i=0; i<6; i++)
-    {
-        arr[i]=arr[i+1];
-    }
-    arr[5]=temp;
+//     for(int i=0; i<6; i++)
+//     {
+//         arr[i]=arr[i+1];
+//     }
+//     arr[5]=temp;
 
-    for(int i=0; i<6; i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-}
+//     for(int i=0; i<6; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+// }
