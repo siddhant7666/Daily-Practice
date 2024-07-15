5e170fcf33cc88d5075d3e474e314cafff7ce531
Siddhant <dearsidd2017@gmail.com>
2024-07-15 20:22:22 +0530

    shifting elements left

--- Test.cpp
+++ Test.cpp
@@ -180,3 +180,22 @@
 //         cout<<arr[i]<<" ";
 //     }
 // }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6]={10,20,30,40,50,60};
+    int temp = arr[0];
+
+    for(int i=0; i<6; i++)
+    {
+        arr[i]=arr[i+1];
+    }
+    arr[5]=temp;
+
+    for(int i=0; i<6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}
