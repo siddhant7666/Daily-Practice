1f9799e073c431c35c6673b44c69d2022271c75f
Siddhant <dearsidd2017@gmail.com>
2024-07-15 18:55:46 +0530

    triplets-solved-test

--- Test.cpp
+++ Test.cpp
@@ -64,17 +64,36 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3]={1,2,3};
+//     for(int i=0; i<3; i++)
+//     {
+//         for(int j=0; j<3; j++)
+//         {
+//             cout<<"["<<" "<<arr[i]<<","<<arr[j]<<" "<<"]";
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
 #include<iostream>
 using namespace std;
 int main()
 {
-    int arr[3]={1,2,3};
+    int arr[3]={10,20,30};
     for(int i=0; i<3; i++)
     {
         for(int j=0; j<3; j++)
         {
-            cout<<"["<<" "<<arr[i]<<","<<arr[j]<<" "<<"]";
+            for(int k=0; k<3; k++)
+            {
+                cout<<"["<<" "<<arr[i]<<","<<arr[j]<<","<<arr[k]<<" "<<"]";
+            }
+            cout<<endl;
         }
-        cout<<endl;
     }
 }
