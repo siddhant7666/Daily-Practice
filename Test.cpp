16b01ca873c7ffb34c1339f08b817430eac5621a
Siddhant <dearsidd2017@gmail.com>
2024-07-15 20:20:18 +0530

    shifting element by two

--- Test.cpp
+++ Test.cpp
@@ -157,3 +157,26 @@
 //     }
 
 // }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[6]={10,20,30,40,50,60};
+//     int temp = arr[5];
+//     int temp2 = arr[4];
+
+//     for(int i=5; i>=0; i--)
+//     {
+//         arr[i]=arr[i-2];
+//     }
+
+//     arr[0]=temp2;
+//     arr[1]=temp;
+
+
+//     for(int i=0; i<6; i++)
+//     {
+//         cout<<arr[i]<<" ";
+//     }
+// }
