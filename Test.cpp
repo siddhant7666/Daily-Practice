12d4d5e71f6ff7de03cf08599e240181dc33d94a
Siddhant <dearsidd2017@gmail.com>
2024-07-18 10:51:55 +0530

    transpose

--- Test.cpp
+++ Test.cpp
@@ -457,32 +457,32 @@
 
 //TRANSPOSE OF A MATRIX
 
-#include<iostream>
-using namespace std;
-int main()
-{
-    int arr[3][3]={
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
-    };
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3][3]={
+//         {1,2,3},
+//         {4,5,6},
+//         {7,8,9}
+//     };
 
-    for(int row =0; row<3; row++)
-    {
-        for(int col =0; col<3; col++)
-        {
-            if(col>=row)
-            swap(arr[row][col],arr[col][row]);
-        }
-    }
+//     for(int row =0; row<3; row++)
+//     {
+//         for(int col =0; col<3; col++)
+//         {
+//             if(col>=row)
+//             swap(arr[row][col],arr[col][row]);
+//         }
+//     }
 
 
-    for(int row=0; row<3; row++)
-    {
-        for(int col=0; col<3; col++)
-        {
-            cout<<arr[row][col]<<" ";
-        }
-        cout<<endl;
-    }
-}
+//     for(int row=0; row<3; row++)
+//     {
+//         for(int col=0; col<3; col++)
+//         {
+//             cout<<arr[row][col]<<" ";
+//         }
+//         cout<<endl;
+//     }
+// }
