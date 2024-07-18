0933ebb38ea8732c08c5179a067578862d3fc764
Siddhant <dearsidd2017@gmail.com>
2024-07-18 10:44:04 +0530

    transpose of a 2d matrix

--- Test.cpp
+++ Test.cpp
@@ -454,3 +454,35 @@
 
 //     cout<<"Sum of principal diagonal sum is: "<<principal_diagonal_sum;
 // }
+
+//TRANSPOSE OF A MATRIX
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[3][3]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    for(int row =0; row<3; row++)
+    {
+        for(int col =0; col<3; col++)
+        {
+            if(col>=row)
+            swap(arr[row][col],arr[col][row]);
+        }
+    }
+
+
+    for(int row=0; row<3; row++)
+    {
+        for(int col=0; col<3; col++)
+        {
+            cout<<arr[row][col]<<" ";
+        }
+        cout<<endl;
+    }
+}
