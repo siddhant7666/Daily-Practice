9f8213107d51df91d2815f604c59feb03a534ad2
Siddhant <dearsidd2017@gmail.com>
2024-07-18 10:28:13 +0530

    rowwise and columnwise sum for 2d array

--- Test.cpp
+++ Test.cpp
@@ -396,3 +396,37 @@
 
 
 // }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3][4]={
+//         {1,2,3,4},
+//         {4,5,6,7},
+//         {6,7,8,9}
+//     };
+//     int row_sum =0;
+
+//     for(int i=0; i<3; i++)
+//     {
+//         for(int j=0; j<4; j++)
+//         {
+//             row_sum+=arr[i][j];
+//         }
+//     }
+
+//     cout<<"Row-wise sum is: "<<row_sum<<endl;
+
+//     int col_sum =0;
+
+//     for(int i=0; i<4; i++)
+//     {
+//         for(int j=0; j<3; j++)
+//         {
+//             col_sum+=arr[j][i];
+//         }
+//     }
+//     cout<<"Column-wise sum is: "<<col_sum;
+// }
