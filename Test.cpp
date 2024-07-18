a4814952e5f87a4d686b21b7668af89c9d09961f
Siddhant <dearsidd2017@gmail.com>
2024-07-18 10:10:10 +0530

    max and min in a 2d array

--- Test.cpp
+++ Test.cpp
@@ -365,3 +365,34 @@
 //     return 0;
 
 // }
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int arr[3][4]={
+//         {1,2,3,4},
+//         {4,5,6,7},
+//         {6,7,8,9}
+//     };
+//     int min = INT_MAX;
+//     int max = INT_MIN;
+
+//     for(int i=0; i<3; i++)
+//     {
+//         for(int j=0; j<4; j++)
+//         {
+//             if(arr[i][j]>max)
+//             max = arr[i][j];
+//             if(arr[i][j]<min)
+//             min = arr[i][j];
+            
+//         }
+//     }
+
+//     cout<<"Max is: "<<max;
+//     cout<<endl;
+//     cout<<"Min is: "<<min;
+
+
+// }
