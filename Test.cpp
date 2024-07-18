7526e371b251c48300a30f27d3d933093009c6ce
Siddhant <dearsidd2017@gmail.com>
2024-07-18 10:53:52 +0530

    Transpose of a non square matrix

--- Test.cpp
+++ Test.cpp
@@ -486,3 +486,40 @@
 //         cout<<endl;
 //     }
 // }
+
+
+//TRANSPOSE IN CASE OF A NON SQUARE
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int arr[3][4] = {
+//         {1, 2, 3, 4},
+//         {5, 6, 7, 8},
+//         {9, 10, 11, 12}
+//     };
+
+//     int transpose[4][3];
+
+//     // Transpose the matrix
+//     for(int row = 0; row < 3; row++)
+//     {
+//         for(int col = 0; col < 4; col++)
+//         {
+//             transpose[col][row] = arr[row][col];
+//         }
+//     }
+
+//     // Print the transposed matrix
+//     for(int row = 0; row < 4; row++)
+//     {
+//         for(int col = 0; col < 3; col++)
+//         {
+//             cout << transpose[row][col] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
