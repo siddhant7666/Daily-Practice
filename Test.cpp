f863629578b52f54bee2292fc7002b102992de0e
Siddhant <dearsidd2017@gmail.com>
2024-07-19 09:00:14 +0530

    negative num to the left

--- Test.cpp
+++ Test.cpp
@@ -524,4 +524,32 @@
 //     return 0;
 // }
 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6] ={12,10,-2,-33,24,-5};
+    int index=0;
+    int left=0;
 
+    while(index<6)
+    {
+        if(arr[index]>0)
+        {
+            index++;
+        }
+        else
+        {
+            swap(arr[index],arr[left]);
+            left++;
+            index++;
+        }
+
+    }
+
+    for(int i=0; i<6; i++)
+    {
+        cout<<" "<< arr[i]<<" ";
+    }
+
+}
