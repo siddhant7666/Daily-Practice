496cf75330c80e7fc9e3f8696e5e6a371d972824
Siddhant <dearsidd2017@gmail.com>
2024-07-29 11:42:52 +0530

    solving wave print in a 2d matrix

--- Test.cpp
+++ Test.cpp
@@ -763,3 +763,26 @@
 //     }
 
 // }
+
+// WAVE PRINT A MATRIX
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    int arr[3][4] ={
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+
+    
+    
+
+}
