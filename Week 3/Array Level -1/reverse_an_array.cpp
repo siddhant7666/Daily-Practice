0b5939dfebb8fc41490a4746b6954d8069d7f592
Siddhant <dearsidd2017@gmail.com>
2025-01-12 16:12:30 +0530

    reversing an array

--- Week 3/Array Level -1/reverse_an_array.cpp
+++ Week 3/Array Level -1/reverse_an_array.cpp
@@ -2,21 +2,19 @@
 using namespace std;
 int main()
 {
-    int arr[5]={9,0,2,3,1};
+    int arr[6]={9,0,2,3,4,1};
     int start =0;
-    int end = 4;
-    while(start<=end)
+    int end = 5;
+    while(start<end)
     {
         swap(arr[start],arr[end]);
         start++;
         end--;
     }
 
-    cout<<"Reversed array is: ";
-
-    for(int i=0; i<5; i++)
+    cout<<"New array is :"<<endl;
+    for(int i=0;i<6;i++)
     {
-        
         cout<<arr[i]<<" ";
     }
 }
