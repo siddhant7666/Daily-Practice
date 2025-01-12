023e8679f953db1d94300570d9e9dbc9a6ada828
Siddhant <dearsidd2017@gmail.com>
2025-01-12 16:21:29 +0530

    extreme print an array

--- Week 3/Array Level -1/extreme_print.cpp
+++ Week 3/Array Level -1/extreme_print.cpp
@@ -2,20 +2,26 @@
 using namespace std;
 int main()
 {
-    int arr[6] = {20, 4, 15, 2, 6, 2};
-    int start = 0;
-    int end = 5;
-    while (start <= end)
-    {
-        if (start == end)
-            cout << arr[start] << " ";
-        else
+    int arr[5] = {20, 4, 15, 2, 6};
+    int start   = 0;
+    int end     = 4;
+    while(start<=end)
         {
-            cout << arr[start] << " ";
-            cout << arr[end] << " ";
-        }
+            if(arr[start]==arr[end])
+            {
+                cout<<arr[start]<<" ";
+            
+            }
+            else
+            {
+            cout<<arr[start]<<" ";
+            start++;
+            cout<<arr[end]<<" ";
+            end--;
 
-        start++;
-        end--;
-    }
+            }
+            
+
+        }
+       
 }
