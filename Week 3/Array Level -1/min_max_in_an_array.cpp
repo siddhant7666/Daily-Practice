b220d709424d3715d9c873faa80cbbb571c9a83d
Siddhant <dearsidd2017@gmail.com>
2025-01-12 16:09:23 +0530

    min and max in an array

--- Week 3/Array Level -1/min_max_in_an_array.cpp
+++ Week 3/Array Level -1/min_max_in_an_array.cpp
@@ -3,19 +3,18 @@ using namespace std;
 int main()
 {
     int arr[10]={1,2,3,9,0,-2,-19,22,98,0};
-    int max=INT_MIN;
-    int min=INT_MAX;
+    int min = INT_MAX;
+    int max = INT_MIN;
     for(int i=0; i<10; i++)
     {
-        if(arr[i]>=max)
-        {
-            max = arr[i];
-        }
-        else if(arr[i]<=min)
+        if(arr[i]<min)
         {
             min = arr[i];
         }
-
+        if(arr[i]>max)
+        {
+            max = arr[i];
+        }
     }
     cout<<"Minimum number in this array is: "<<min<<endl;
     cout<<"Maximum number in this array is: "<<max;
