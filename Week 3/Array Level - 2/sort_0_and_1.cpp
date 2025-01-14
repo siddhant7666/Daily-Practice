b8199d0cf4b2ea5e25f9fef02807ab2fa96a42f5
Siddhant <dearsidd2017@gmail.com>
2025-01-14 10:49:13 +0530

    sorting 0 and 1-> brute force and two pointer

--- Week 3/Array Level - 2/sort_0_and_1.cpp
+++ Week 3/Array Level - 2/sort_0_and_1.cpp
@@ -8,31 +8,95 @@ void sort_array(int arr[], int n)
 
     //2nd approach : TWO POINTER METHOD -> TC : 0(N) , SC : 
 
-    int start =0;
+    // int start =0;
+    // int end = n-1;
+
+    // while(start<end)
+    // {
+    //     if(arr[start]==0 )
+    //     {
+    //         start++;
+    //     }
+    //     else if (arr[end]==1)
+    //     {
+    //         end--;
+    //     }
+    //     else if( arr[start]==1 && arr[end]==0) // else
+    //     {
+    //         swap(arr[start],arr[end]);
+    //         start++;
+    //         end--;  
+    //     }
+       
+    // }
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout<<" "<<arr[i]<<" ";
+    // }
+
+    //1st approach -> count zeroes and ones
+
+    
+    // int zero_count =0;
+    // int one_count =0;
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     if(arr[i]==0)
+    //     {
+    //         zero_count++;
+    //     }
+    //     else
+    //     {
+    //         one_count++;
+    //     }
+    // }
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     if(i<zero_count)
+    //     {
+    //         arr[i] = 0;
+    //     }
+    //     else
+    //     {
+    //         arr[i] = 1;
+    //     }
+    // }
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout<<" "<<arr[i]<<" ";
+    // }
+
+    //2nd approach -> two pointer method
+
+    int start = 0;
     int end = n-1;
 
     while(start<end)
     {
-        if(arr[start]==0 )
+        if(arr[start]==0)
         {
             start++;
+
         }
-        else if (arr[end]==1)
+        else if(arr[end]==1)
         {
             end--;
         }
-        else if( arr[start]==1 && arr[end]==0) // else
+        else if(arr[start] ==1&&arr[end]==0)
         {
             swap(arr[start],arr[end]);
             start++;
-            end--;  
+            end--;
         }
-       
     }
 
     for(int i=0; i<n; i++)
     {
-        cout<<" "<<arr[i]<<" ";
+        cout<<arr[i]<<" ";
     }
 
 }
