5f45cc041e436ea8b8ac1b7da46af8857b1b91b2
Siddhant <dearsidd2017@gmail.com>
2025-01-14 10:56:27 +0530

    shift by one right

--- Week 3/Array Level - 2/HW Level - 2/Shift_by_one_Right.cpp
+++ Week 3/Array Level - 2/HW Level - 2/Shift_by_one_Right.cpp
@@ -1,32 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// PRINT FUNCTION - ARRAY KO PRINT KARNE KE LIYE
-void print(int arr[], int n)
-{
-    for(int i=0; i<n; i++)
-    {
-        cout<<" "<<arr[i]<<" ";
-    }
-}
-
-// SHIFT FUNCTION -> ARRAY KE ELEMENT KO EK PLACE RIGHT SHIFT KARNE KE LIYE 
 void shift(int arr[], int n)
 {
     int temp = arr[n-1];
     for(int i = n-2; i>=0; i--)
     {
-        arr[i+1]=arr[i];
+        arr[i+1]= arr[i];
     }
-    arr[0]=temp;
+    arr[0] = temp;
 
-    print(arr,10);
-
+    for(int i = 0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
-
-
 int main()
 {
-    int arr[10] = {3,4,5,7,6,6,4,3,8,9};
-    shift(arr,10);
+    int arr[10] = {0,1,2,3,4,5,6,7, 8, 9};
+    shift(arr, 10);
+
+
 }
