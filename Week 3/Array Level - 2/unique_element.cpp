e2b57248aa2d71bcc8ff8f4ebd03556a63984244
Siddhant <dearsidd2017@gmail.com>
2025-01-14 10:31:25 +0530

    unique element by 3 approaches, by brute force, hashmap and xor

--- Week 3/Array Level - 2/unique_element.cpp
+++ Week 3/Array Level - 2/unique_element.cpp
@@ -1,38 +1,65 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int find_unique(int arr[], int n)
+// 1st approach -> looping and looking for 2nd occurenece
+// int unique(int arr[], int n)
+// {
+//     for(int i=0; i<n; i++)
+//     {
+//         bool present = false;
+//         for(int j =0; j<n; j++)
+//         {
+//             if(i!=j)
+//             {
+//                 if(arr[i]==arr[j])
+//                 {
+//                     present = true;
+//                 }
+//             }
+//         }
+//         if(present==false)
+//         {
+//             return arr[i];
+//         }
+//     }
+// }
+
+// 2nd approach -> using hashmap
+
+// int unique(int arr[], int n)
+// {
+//     unordered_map<int, int> freq;
+//     for(int i=0; i<n; i++)
+//     {
+//         freq[arr[i]]++;
+//     }
+//     for(int i=0; i<n; i++)
+//     {
+//         if(freq[arr[i]]==1)
+//         {
+//             return arr[i];
+//         }
+//     }
+// }
+
+//3rd approach -> using XOR
+
+int unique(int arr[], int n)
 {
-    int ans =0;
+    int ans = 0;
     for(int i=0; i<n; i++)
     {
         ans = ans^arr[i];
     }
-
-    /*
-Let us consider the above example.  
-Let ^ be xor operator as in C and C++.
-
-res = 7 ^ 3 ^ 5 ^ 4 ^ 5 ^ 3 ^ 4
-
-Since XOR is associative and commutative, above 
-expression can be written as:
-res = 7 ^ (3 ^ 3) ^ (4 ^ 4) ^ (5 ^ 5)  
-    = 7 ^ 0 ^ 0 ^ 0
-    = 7 ^ 0
-    = 7 
-*/
-
-
     return ans;
-
-
 }
 
 
 int main()
 {
-    int arr[5]={2,1,2,3,1};
-   int ans =  find_unique(arr,5);
-   cout<<"Unique element is: "<<ans;
+    int arr[7] = {1,2,3,1,2, 3, 5};
+    int ans = unique(arr,7);
+    cout<<"Unique element is: "<<ans;
+
+
 }
