8f16629a7a9377da68d399a98902c91423e6551d
Siddhant <dearsidd2017@gmail.com>
2025-01-15 09:58:09 +0530

    transpose of a matrix

--- Week 3/Array Level - 3/Transpose_2D_Matrix.cpp
+++ Week 3/Array Level - 3/Transpose_2D_Matrix.cpp
@@ -1,26 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+void transpose(int arr[3][3], int m, int n)
+{
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            if(j>=i)
+            {
+                swap(arr[i][j], arr[j][i]);
+            }
+        }
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+}
+
+
 int main()
 {
-    int arr[][3]={
+    int arr [3][3] ={
         {1,2,3},
         {4,5,6},
         {7,8,9}
     };
-    for(int row =0; row<3; row++)
-    {
-        for(int col =row; col<3; col++)
-        {
-           swap(arr[row][col],arr[col][row]); 
-        }
-    }
+    transpose(arr, 3, 3);
 
-    for(int row=0; row<3; row++)
-    {
-        for(int col=0; col<3; col++)
-        {
-            cout<<arr[row][col]<<" ";
-        }
-        cout<<endl;
-     }
 }
