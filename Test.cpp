65eefb1406cd41fac3b4cc77855280d63f568713
Siddhant <dearsidd2017@gmail.com>
2025-02-12 12:21:25 +0530

    binary search done but, there is a bug so, now I will debug this one

--- Test.cpp
+++ Test.cpp
@@ -1413,7 +1413,28 @@ using namespace std;
 
 int binarysearch(int arr[], int n, int target)
 {
-    
+    int start = 0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+    while(start<=end)
+    {
+        mid = start + (end-start)/2;
+        if(arr[mid]==target)
+        {
+            return mid;
+
+        }
+        else if(arr[mid]>target)
+        {
+            end = mid-1;
+        }
+        else
+        {
+            start = mid+1;
+        }
+        
+    }
+    return -1;
 }
 
 int main()
@@ -1425,6 +1446,14 @@ int main()
     int n = sizeof(arr)/sizeof(arr[0]);
     int target = -9;
     int result = binarysearch(arr, n, target);
+    if(result==-1)
+    {
+        cout<<"Element not found!!!";
+    }
+    else
+    {
+        cout<<"Element found at index: "<<result;
+    }
 
 
 }
